9b1d34db50a0f2b004f4544ddbd7dcd20ac65a7d
Matthew-Osaruyi-Eruogun <eruomatt@gmail.com>
2026-06-05 00:29:04 +0100

    fix: add guard clause to productList initialization on homepage to prevent null error

--- src/js/main.js
+++ src/js/main.js
@@ -5,17 +5,22 @@ import { loadHeaderFooter, showWelcomeBanner } from "./utils.mjs";
 import Alert from "./Alert.js";
 
 const productData = new ProductData("tents");
-const productList = new ProductList(
-  "tents",
-  productData,
-  document.querySelector(".product-list"),
-);
+const listElement = document.querySelector(".product-list");
+
+// Only creates the product list instance if the target element exists in the DOM
+let productList = null;
+if (listElement) {
+  productList = new ProductList("tents", productData, listElement);
+}
 
 async function init() {
   await loadHeaderFooter();
   updateCartCount();
 
-  productList.init();
+  // Guard clause: Only initialize if productList was successfully created
+  if (productList) {
+    productList.init();
+  }
 
   const alert = new Alert("/json/alerts.json");
   alert.loadAlerts();
